082beaa631b9dadee2f49b7f6e2cf263a51f2223
silva01 <silva20102@gmail.com>
2022-04-14 23:03:33 -0300

    #11 - Desfazendo refatoracao

--- src/main/kotlin/br/net/silva/daniel/ex/plan/model/Excel.kt
+++ src/main/kotlin/br/net/silva/daniel/ex/plan/model/Excel.kt
@@ -1,10 +1,7 @@
 package br.net.silva.daniel.ex.plan.model
 
 interface Excel {
-    fun title() : Boolean
+    fun title() : String
     fun value() : Any
     fun position() : Int
-    fun addTitle(title : String)
-    fun addValue(value : Any)
-    fun addPosition(position : Int)
 }
