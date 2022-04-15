b4d6e55cd6bd2a959e928208e6a8d23a55fd2941
silva01 <silva20102@gmail.com>
2022-04-14 22:53:19 -0300

    #11 - Refatorado Interface para geração do Excel

--- src/main/kotlin/br/net/silva/daniel/ex/plan/model/Excel.kt
+++ src/main/kotlin/br/net/silva/daniel/ex/plan/model/Excel.kt
@@ -1,7 +1,10 @@
 package br.net.silva.daniel.ex.plan.model
 
 interface Excel {
-    fun title() : String
+    fun title() : Boolean
     fun value() : Any
     fun position() : Int
+    fun addTitle(title : String)
+    fun addValue(value : Any)
+    fun addPosition(position : Int)
 }
